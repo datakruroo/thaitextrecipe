ff6ced7a819115f9406b340d39a721a33a3de688
dayakruroo <siwachoat.s@gmail.com>
2024-04-14 19:26:41 +0700

    add thai_stopwords() function

---
+++ R/thai_stopwords.R
@@ -0,0 +1,19 @@
+
+thai_stopwords <- function(){
+  suppressPackageStartupMessages(library(dplyr))
+  if (!"pythainlp" %in% ls() ) {
+    library(reticulate)
+    pythainlp<-import("pythainlp")
+  }
+  
+thai_stopword_fn <- "
+def thai_stopword():
+  import pythainlp
+  stopword = pythainlp.corpus.thai_stopwords() ## frozenset
+  stopword = list(stopword)
+  return stopword
+"
+py_run_string(thai_stopword_fn)
+th_stopwords <- tibble(word = py$thai_stopword(), lexicon = "PyThaiNLP")
+return(th_stopwords)
+}
